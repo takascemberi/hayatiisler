72697a0630adba976291c3ec06a66d29bcec1f6c
takascemberi okeycini <takascemberi@gmail.com>
2025-07-05 14:12:05 +0000

    Başlık ayrıldı, renk geçişli yazı ve alt açıklama kutuları eklendi

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/no-unescaped-entities': 'off'
+  
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/* * @type {import('next').nextConfig } */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+module.exports = nextConfig;
